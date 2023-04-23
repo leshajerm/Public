d345517ab28ce9980e38716788adb862c6dedb03
leshajerm <leshajerm@gmail.com>
2023-04-22 21:22:16 -0400

    Public

---
+++ snail.js
@@ -0,0 +1,18 @@
+//                                         Snail
+// let dayDistanse = 3;
+// let nightDistance = -2;
+// let distance = 0;
+// let days = 0;
+// let wall = 5;
+// while ((distance += dayDistanse) <= wall) {
+//     days++;
+//     console.log(`current distance at the end of the day for day ${days} is ${distance} metr(s)`);
+//     if (distance == wall)
+//     {
+// console.log("finish");
+//     }
+//     else {
+//       console.log("go foward")
+//       distance+=nightDistance;  
+//     }
+//   }
